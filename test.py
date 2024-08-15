c3e0bf3c69a50157bc4d6e50cbecb1f202161866
Gulsahbb <gulsahbaskurt.10@gmail.com>
2024-08-15 14:45:02 +0300

    created test python file

---
+++ test.py
@@ -0,0 +1,9 @@
+import time
+import random
+from tkinter import *
+
+pencere = Tk()
+pencere.title("Taş Kağıt Makas Oyunu")
+pencere.config(bg = "black")
+pencere.geometry("500x300")
+pencere.mainloop()
