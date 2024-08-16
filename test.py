ea5ec2dca8b7e57a67a7812b43e034b0b1952e97
Gulsahbb <gulsahbaskurt.10@gmail.com>
2024-08-16 16:31:11 +0300

    deleted

--- test.py
+++
@@ -1,9 +0,0 @@
-import time
-import random
-from tkinter import *
-
-pencere = Tk()
-pencere.title("Taş Kağıt Makas Oyunu")
-pencere.config(bg = "black")
-pencere.geometry("500x300")
-pencere.mainloop()
