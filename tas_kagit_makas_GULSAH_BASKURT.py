f925b53258e48adaafad5709d959dd8453c1ebee
Gulsahbb <gulsahbaskurt.10@gmail.com>
2024-08-17 18:50:46 +0300

    mesaj

--- tas_kagit_makas_GULSAH_BASKURT.py
+++ tas_kagit_makas_GULSAH_BASKURT.py
@@ -1,7 +1,22 @@
 # Rastgele seçimler yapmak için kullanıldı.
 import random
 
+mesaj = """
+          
+############# TAŞ KAĞIT MAKAS OYUNU #############
+
+1 - Taş, makası yener.
+2 - Makas, kağıdı yener.
+3 - Kağıt, taşı yener.
+4 - İlk iki turu kazanan oyunu kazanır.
+Her oyun sonrası bilgisayar ve oyuncuya oynamak
+veya çıkmak isteği sorulur.
+Her iki tarafta oynamak isterse oyun devam eder.
+Oyundan çıkmak istiyorsanız 'exit' yazın.
+
+################# İYİ EĞLENCELER #################
+    """
 def tas_kagit_makas_GULSAH_BASKURT():
-    pass
+    print(mesaj)
 
 tas_kagit_makas_GULSAH_BASKURT()
