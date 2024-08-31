4bb3cd4d1f06d5aa1d389b3a7dc99c6131777ec5
Gulsahbb <gulsahbaskurt.10@gmail.com>
2024-08-31 09:40:47 +0300

    Updated

--- tas_kagit_makas.py
+++ tas_kagit_makas.py
@@ -4,3 +4,13 @@ import pygame
 import random
 # Sistemle ilgili işlevleri sağlayan kütüphane.
 import sys
+
+
+# Pygame kütüphanesin başlatıldı.
+pygame.init()
+
+# Ekran ayarları
+screen_width, screen_height = 1000, 400 # Ekranın genişlik ve yükseklik değerlerini belirlendi.
+screen = pygame.display.set_mode((screen_width, screen_height)) # Pygame ekran penceresi oluşturuldu.
+pygame.display.set_caption('Kozmik Düello: Taş-Kâğıt-Makas') # Ekran başlığı ayarlanıldı.
+
