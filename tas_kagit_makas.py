808e7a777b8880e3eedb745c7881d0fd6fd79599
Gulsahbb <gulsahbaskurt.10@gmail.com>
2024-08-31 09:39:05 +0300

    Updated

---
+++ tas_kagit_makas.py
@@ -0,0 +1,6 @@
+# Oyun geliştirmek için kullanılan bir kütüphane.
+import pygame
+# Rastgele seçimler yapmak için kullanıldı.
+import random
+# Sistemle ilgili işlevleri sağlayan kütüphane.
+import sys
