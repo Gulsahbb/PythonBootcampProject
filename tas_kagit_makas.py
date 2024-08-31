1c073869d5504f345eef4f75cb434aac7368b63b
Gulsahbb <gulsahbaskurt.10@gmail.com>
2024-08-31 09:41:17 +0300

    Updated

--- tas_kagit_makas.py
+++ tas_kagit_makas.py
@@ -14,3 +14,13 @@ screen_width, screen_height = 1000, 400 # Ekranın genişlik ve yükseklik değe
 screen = pygame.display.set_mode((screen_width, screen_height)) # Pygame ekran penceresi oluşturuldu.
 pygame.display.set_caption('Kozmik Düello: Taş-Kâğıt-Makas') # Ekran başlığı ayarlanıldı.
 
+# Renkler
+DARK_BLUE = (20, 33, 61)
+DARK_YELLOW = (252, 163, 17)
+GREY = (229, 229, 229)
+DARK_RED = (255,176,176)
+DARK_GREEN = (252,103,54)
+
+# Font
+font = pygame.font.SysFont(None, 24) # Sistem fontunu kullanarak belirli bir font boyutu (24) ayarlamdı.
+
